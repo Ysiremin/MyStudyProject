4d5a13fa38bcbad54b654623e3f9406c52934860
Yashiru <yasireminciftci@gmail.com>
2021-03-22 15:11:08 +0300

    Add project files.

---
+++ Entities/Abstrack/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstrack
+{
+    //IEntitiy implement eden class bir veritabanı tablosudur
+    public interface IEntity
+    {
+    }
+}
