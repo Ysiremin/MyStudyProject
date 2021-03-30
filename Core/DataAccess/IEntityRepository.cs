daf73c472cfac2ce3cd81e1ab5d6cdf6f5c31c13
Yashiru <yasireminciftci@gmail.com>
2021-03-30 17:01:19 +0300

    Update (30.03.2021)

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,13 +1,13 @@
-﻿using Entities.Abstrack;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstrack
+namespace Core.DataAccess
 {
     //Generic Constraint
-    //class :
+    //class : referans tip
     //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir.
     //new() : new'lenebilir olmalı
     public interface IEntityRepository<T> where T:class, IEntity, new()
